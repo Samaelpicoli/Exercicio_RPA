b6a6201e81e46a5505370de25dd2e75ffee56619
SamaelPicoli <76179103+Samaelpicoli@users.noreply.github.com>
2024-02-19 10:47:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#BIBLIOTECAS UTILIZADAS NO PROCESSO
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -6,18 +7,58 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 import pandas as pd
+import requests
 
+#MÓDULOS UTILIZADOS NO PROCESSO
+from config import usuario, senha
+import processo 
+import pega_arquivo as get
 
-from config import *
-import process 
+status_loop = 'ON'
+estado = 'INITIALIZATION'
 
-#leitura do arquivo csv que será usado para inserir os dados
-df = pd.read_csv('datatable.csv', sep=',')
+while status_loop == 'ON':
 
+    match estado:
 
-process.login(driver, wdw, username, password)
-process.inserir_dados(driver, wdw, df)
-process.avaliar_dados(driver, wdw, df)
+        case 'INITIALIZATION':
+            try:
+                options = webdriver.ChromeOptions()
+                options.add_argument("--disable-notifications")
+                driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+                wdw = WebDriverWait(driver, 40)
+                processo.login(driver, wdw, usuario, senha)
+                estado = 'GET TRANSACTION'
+                continue
+            except Exception as error:
+                print(f'Erro durante a inicialização: {error}')
+                estado = 'END'
+                continue
+            
+        case 'GET TRANSACTION':
+            try:
+                nome_arquivo = get.solicitar_csv()
+                arquivo = pd.read_csv(nome_arquivo, sep=',')
+                estado = 'PROCESS'
+                continue
+            except Exception as error:
+                print(f'Erro durante a captura dos dados: {error}')
+                estado = 'END'
+                continue
 
-print('Finalizado!')
-driver.close()
+        case 'PROCESS':
+            try:
+                processo.inserir_dados(driver, wdw, arquivo)
+                processo.avaliar_dados(driver, wdw, arquivo)
+                estado = 'END'
+                continue
+            except Exception as error:
+                print(f'Erro durante o processamento: {error}')
+                estado = 'END'
+                continue
+
+        case 'END':
+            print('Finalizado!')
+            driver.close()
+            status_loop = 'OFF'
+            continue
